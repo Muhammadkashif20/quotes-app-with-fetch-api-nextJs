bbaac96f4ed70427b754882a21345c4a5545d2f5
muhammadkashif20 <kashif28@gmail.com>
2024-11-11 21:25:28 +0500

    Working In Show Cards With Fetch Api

---
+++ src/app/components/QuotesCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuotesCard = () => {
+  return (
+    <div>
+      <h1 className='font-bold text-2xl text-center'>Quotes Card</h1>
+    </div>
+  )
+}
+
+export default QuotesCard
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 import Link from "next/link"
-
-const page = () => {
+import QuotesCard from "./components/quotesCard"
+const page = async () => {
+  let quotes= await fetch('https://dummyjson.com/quotes')
+  quotes= await quotes.json()
+console.log('quotes=>',quotes);
   return (
     <div>
-      <h1>Hello World</h1>
-      <Link href="/quotes">
-      <button>Quotes</button>
-      </Link>
+      <h1 className="font-bold text-center text-2xl">Quotes App</h1>
+      {quotes.map((data)=>{
+        return(
+          <div>
+            <QuotesCard/>
+          </div>
+        )
+      })}
     </div>
   )
 }
