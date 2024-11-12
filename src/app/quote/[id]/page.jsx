1074247c2a64886ffa18380b9a034b10bd143590
muhammadkashif20 <kashif28@gmail.com>
2024-11-12 17:30:34 +0500

    Few Changes on NotFound Page

--- src/app/quote/[id]/page.jsx
+++ src/app/quote/[id]/page.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const QuoteCards =async ({params}) => {
     let singleQuotes= await fetch(`https://dummyjson.com/quotes/${params.id}`)
     singleQuotes=await singleQuotes.json()
-    if(!singleQuotes){
+    if(singleQuotes.message){
         notFound()
     }
   return (
--- src/app/quote/not-found.js
+++ src/app/quote/not-found.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
 
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-6xl font-bold text-gray-800">404</h1>
       <p className="text-xl text-gray-600">Page Not Found</p>
+      <Link href={'/'}>
+      <button
+        className="mt-4 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+        Go to Home
+      </button>
+          </Link>
     </div>
   );
 };
