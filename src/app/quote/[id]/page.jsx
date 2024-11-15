fc777b7cfb68f65527afb5074d1665c41ee11eb4
muhammadkashif20 <kashif28@gmail.com>
2024-11-15 16:24:14 +0500

    vercel issue ""

--- src/app/quote/[id]/page.jsx
+++ src/app/quote/[id]/page.jsx
@@ -16,7 +16,7 @@ const QuoteCards = async ({ params }) => {
       <h1 className="text-center font-bold text-3xl mb-6 text-gray-800">Single Quote 📄</h1>
       <div className="border border-gray-300 p-6 rounded-lg shadow-md bg-white mx-auto max-w-3xl">
         <h1 className="font-semibold text-2xl mb-4 text-gray-700">
-          "{singleQuotes.quote}"
+          {singleQuotes.quote}
         </h1>
         <h1 className="text-md text-gray-600">
           - <span className="text-blue-500 font-medium">{singleQuotes.author}</span>
