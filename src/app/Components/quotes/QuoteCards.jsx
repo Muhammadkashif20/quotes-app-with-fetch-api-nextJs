50abe7f57405a0bc1dc68e5eda6420f306da62a5
muhammadkashif20 <kashif28@gmail.com>
2024-11-15 16:29:37 +0500

    vercel issues deploy like "" solve

--- src/app/Components/quotes/QuoteCards.jsx
+++ src/app/Components/quotes/QuoteCards.jsx
@@ -4,10 +4,10 @@ const QuoteCards = ({ author, quote }) => {
   return (
     <div className="border border-gray-200 m-5 p-6 rounded-lg shadow-sm bg-white">
       <h1 className="font-semibold text-xl mb-3 text-gray-800">
-        {quote}
+        {`"${quote}"`}
       </h1>
       <h1 className="text-md text-gray-600">
-        - <span className="text-blue-500 font-medium">{author}</span>
+      {'-'}  <span className="text-blue-500 font-medium">{author}</span>
       </h1>
     </div>
   );
